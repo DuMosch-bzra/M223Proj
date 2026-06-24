c7f00613ca04fac2a9e64b4c4d2021e198d4f427
daniel-bzra <daniel.sigg@bwz-rappi.ch>
2026-06-24 14:53:56 +0200

    Off stream coding trust

--- src/main/java/ch/glauserillnau/auftragsverwaltung/enums/DocumentType.java
+++
@@ -1,9 +0,0 @@
-package ch.glauserillnau.auftragsverwaltung.enums;
-
-public enum DocumentType {
-    ORDER_DOCUMENT,
-    REPORT,
-    INVOICE,
-    IMAGE,
-    OTHER
-}
--- src/main/java/ch/glauserillnau/auftragsverwaltung/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package ch.glauserillnau.auftragsverwaltung.enums;
-
-public enum Role {
-    ADMIN,
-    MANAGER,
-    EMPLOYEE
-}
